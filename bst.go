0290707d433f726b507d5bc7e5c113ede5fc1c17
James A. Smith <jasmith86@gmail.com>
2019-06-26 22:19:30 -0400

    separate main and bst

--- bst.go
+++ bst.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
 
 // Tree interface TODO?
@@ -95,31 +94,4 @@ func (n *Node) Min() (int, error) {
 		return n.left.Min()
 	}
 	return n.val, nil
-}
-
-func main() {
-	list := []int{1, 2, 0, -1, 0, 0, 0, 0, 0, 1000, 9999}
-
-	t := &BST{}
-	for _, v := range list {
-		if _, err := t.Insert(v); err != nil {
-			fmt.Println(err, v)
-		} else {
-			fmt.Println("added", v)
-		}
-	}
-
-	fmt.Println("Height", t.head.Height())
-
-	if l, err := t.head.Max(); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Max", l)
-	}
-
-	if s, err := t.head.Min(); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Min", s)
-	}
 }
--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World")
+	list := []int{1, 2, 0, -1, 0, 0, 0, 0, 0, 1000, 9999}
+
+	t := &BST{}
+	for _, v := range list {
+		if _, err := t.Insert(v); err != nil {
+			fmt.Println(err, v)
+		} else {
+			fmt.Println("added", v)
+		}
+	}
+
+	fmt.Println("Height", t.head.Height())
+
+	if l, err := t.head.Max(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Max", l)
+	}
+
+	if s, err := t.head.Min(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Min", s)
+	}
 }
