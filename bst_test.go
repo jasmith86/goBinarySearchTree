9d2b76d109ecdd21a53ca7f54d8f09931262afc9
James A. Smith <jasmith86@gmail.com>
2019-06-27 17:30:45 -0400

    test InOrder

--- bst_test.go
+++ bst_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -147,6 +149,63 @@ func TestBSTMinMax(t *testing.T) {
 					t.Errorf("wrong Max(). got %v, want %v, from %v", got, want, test.input)
 				}
 			}
+		})
+	}
+}
+
+func TestInOrder(t *testing.T) {
+	tests := []struct {
+		name        string
+		tree        *Node
+		input       []int
+		expectError bool
+	}{
+		{name: "empty root", input: []int{}, expectError: true},
+		{name: "single node", input: []int{9}},
+		{name: "height 2", input: []int{2, 0, 3}},
+		{name: "height 3", input: []int{3, 2, 1}},
+		{name: "height 3 all left", input: []int{3, 2, 1}},
+		{name: "height 4 all right", input: []int{1, 2, 3, 4}},
+		{name: "height 3 full", input: []int{10, 8, 7, 9, 12, 11, 13}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.tree = &Node{}
+			for _, v := range test.input {
+				if err := test.tree.Insert(v); err != nil {
+					t.Errorf("failed to Insert: %v: %v", v, err)
+				}
+			}
+			// Test Min
+			got := test.tree.InOrder()
+			sort.Ints(test.input)
+			want := test.input
+			if !reflect.DeepEqual(want, got) {
+				if !(len(want) == 0 && len(got) == 0) { // TODO DeepEqual on empty slices
+					t.Errorf("wrong InOrder(). got %+v, want %+v", got, want)
+				}
+			}
+			//gotErr := err != nil
+			//if test.expectError != gotErr {
+			//	t.Errorf("error expectation mismatch in. got %v from %v", got, test.input)
+			//}
+			//if len(test.input) > 0 {
+			//	if want := minInt(test.input...); want != got {
+			//		t.Errorf("wrong Min(). got %v, want %v, from %v", got, want, test.input)
+			//
+			//	}
+			//}
+			//// Test Max
+			//got, err = test.tree.Max()
+			//gotErr = err != nil
+			//if test.expectError != gotErr {
+			//	t.Errorf("error expectation mismatch in Max(). got %v from %v", got, test.input)
+			//}
+			//if len(test.input) > 0 {
+			//	if want := maxInt(test.input...); want != got {
+			//		t.Errorf("wrong Max(). got %v, want %v, from %v", got, want, test.input)
+			//	}
+			//}
 		})
 	}
 }
