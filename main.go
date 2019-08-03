130d04a8ef8096fd3fff6d4564538dd23cf09f93
James A. Smith <jasmith86@gmail.com>
2019-08-02 20:20:56 -0400

    cleanup and commenting

--- main.go
+++ main.go
@@ -8,17 +8,13 @@ import (
 // Fulfil Item interface specified in bst.go
 type MyInt int
 
-func (a MyInt) Less(than Item) bool {
-	return a < than.(MyInt)
-}
-func (a MyInt) Greater(than Item) bool {
-	return a > than.(MyInt)
-}
-func (a MyInt) Equals(to Item) bool {
-	return a == to.(MyInt)
-}
+func (a MyInt) Less(than Item) bool    { return a < than.(MyInt) }
+func (a MyInt) Greater(than Item) bool { return a > than.(MyInt) }
+func (a MyInt) Equals(to Item) bool    { return a == to.(MyInt) }
 
-func insertBulk(n *Node, values []MyInt) (int, error) { // todo Remove?
+// insertBulk is a utility function to add a slice of Items, in this case MyInt's.
+// Used heavily in bst_test.go
+func insertBulk(n *Node, values []MyInt) (int, error) {
 	var numInserted int
 	for _, v := range values {
 		err := n.Insert(v)
@@ -35,33 +31,28 @@ func insertBulk(n *Node, values []MyInt) (int, error) { // todo Remove?
 func main() {
 	var list []MyInt
 	list = []MyInt{1, 2, 0, -1, 0, 0, 0, 0, 0, 1000, 9999}
-
 	root := &Node{}
-
-	//var cnt int
-	//for _, v := range list {
-	//	err := root.Insert(v)
-	//	if err != nil {
-	//		fmt.Println(err)
-	//	} else {
-	//		cnt++
-	//	}
-	//}
+	// Insert all the elements in list
 	cnt, err := insertBulk(root, list)
-	_ = err
-	fmt.Println("added", cnt, "values to", root)
+	if err != nil {
+		fmt.Println("Failed to insert", len(list)-cnt, "Items.")
+	}
+	fmt.Println("Added", cnt, "values to", root)
 	fmt.Println("Height", root.Height())
+	// Get max value
 	if l, err := root.Max(); err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println("Max", l)
+		fmt.Println("Max is", l)
 	}
+	// Get min value
 	if s, err := root.Min(); err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println("Min", s)
+		fmt.Println("Min is ", s)
 	}
-	fmt.Println("Count", root.Count())
-	fmt.Println("InOrder", root.InOrder())
-
+	// Get number of elements in the BST
+	fmt.Println("Count is", root.Count())
+	// Get elements according to in-order traversal
+	fmt.Println("InOrder traversal yields", root.InOrder())
 }
